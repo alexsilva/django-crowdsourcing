afea70581622b65a69eca1ce7648685e9f9d010d
alex <geniofuturo@gmail.com>
2017-09-20 10:22:20 -0300

    Correcting warnings.

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.static import serve as serve_view
 
 admin.autodiscover()
 
@@ -11,12 +12,10 @@ urlpatterns = [
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^media/(?P<path>.*)$',
-        'django.views.static.serve',
+    url(r'^media/(?P<path>.*)$', serve_view,
         {'document_root': settings.MEDIA_ROOT}),
     # See settings.py for detailed instructions on how to build the
     # documentation.
-    url(r'^docs/(?P<path>.*)$',
-        'django.views.static.serve',
+    url(r'^docs/(?P<path>.*)$', serve_view,
         {'document_root': settings.DOCUMENTATION_ROOT})
 ]

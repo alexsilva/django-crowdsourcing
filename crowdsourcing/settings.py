cf55ecfbd6355b6069776ad3cd7bc880f71c6f3b
alex <geniofuturo@gmail.com>
2017-10-05 18:00:45 -0300

    Renaming settings var.

--- crowdsourcing/settings.py
+++ crowdsourcing/settings.py
@@ -1,19 +1,19 @@
-from django.conf import settings as _gs
+from django.conf import settings
 
-CROWDSOURCING_APP_NAME = getattr(_gs, "CROWDSOURCING_APP_NAME", "Survey")
+CROWDSOURCING_APP_NAME = getattr(settings, "CROWDSOURCING_APP_NAME", "Survey")
 
 """ This sets the default "Moderate submissions" value of surveys. """
-MODERATE_SUBMISSIONS = getattr(_gs,
+MODERATE_SUBMISSIONS = getattr(settings,
                                'CROWDSOURCING_MODERATE_SUBMISSIONS',
                                False)
 
-IMAGE_UPLOAD_PATTERN = getattr(_gs,
+IMAGE_UPLOAD_PATTERN = getattr(settings,
                                'CROWDSOURCING_IMAGE_UPLOAD_PATTERN',
                                'crowdsourcing/images/%Y/%m/%d')
 
-FLICKR_API_KEY = getattr(_gs, 'CROWDSOURCING_FLICKR_API_KEY', '')
+FLICKR_API_KEY = getattr(settings, 'CROWDSOURCING_FLICKR_API_KEY', '')
 
-FLICKR_API_SECRET = getattr(_gs, 'CROWDSOURCING_FLICKR_API_SECRET', '')
+FLICKR_API_SECRET = getattr(settings, 'CROWDSOURCING_FLICKR_API_SECRET', '')
 
 """
 
@@ -42,14 +42,14 @@ _flickr = flickrapi.FlickrAPI(settings.CROWDSOURCING_FLICKR_API_KEY,
 _flickr.groups_pools_getGroups()
 """
 
-FLICKR_TOKEN = getattr(_gs, 'CROWDSOURCING_FLICKR_TOKEN', '')
+FLICKR_TOKEN = getattr(settings, 'CROWDSOURCING_FLICKR_TOKEN', '')
 
-FLICKR_FROB = getattr(_gs, 'CROWDSOURCING_FLICKR_FROB', '')
+FLICKR_FROB = getattr(settings, 'CROWDSOURCING_FLICKR_FROB', '')
 
 """ Are Flickr photos live by default? """
-FLICKR_LIVE = getattr(_gs, 'CROWDSOURCING_FLICKR_LIVE', False)
+FLICKR_LIVE = getattr(settings, 'CROWDSOURCING_FLICKR_LIVE', False)
 
-FLICKR_TOKENCACHE_PATH = getattr(_gs,
+FLICKR_TOKENCACHE_PATH = getattr(settings,
                                  'CROWDSOURCING_FLICKR_TOKENCACHE_PATH',
                                  '/tmp/flickr_tokencache')
 
@@ -57,41 +57,41 @@ FLICKR_TOKENCACHE_PATH = getattr(_gs,
 # list before rendering. For example, if your user interface has sorting
 # based on votes, you could set this value. Use a python path to a function
 # that takes a submission list and a request object.
-PRE_REPORT = getattr(_gs, 'CROWDSOURCING_PRE_REPORT', '')
+PRE_REPORT = getattr(settings, 'CROWDSOURCING_PRE_REPORT', '')
 
 # If a survey is set to e-mail someone every time someone enters the survey,
 # this will be the return address.
-SURVEY_EMAIL_FROM = getattr(_gs, 'CROWDSOURCING_SURVEY_EMAIL_FROM', None)
+SURVEY_EMAIL_FROM = getattr(settings, 'CROWDSOURCING_SURVEY_EMAIL_FROM', None)
 if SURVEY_EMAIL_FROM is None:
-    SURVEY_EMAIL_FROM = getattr(_gs, 'DEFAULT_FROM_EMAIL', None)
+    SURVEY_EMAIL_FROM = getattr(settings, 'DEFAULT_FROM_EMAIL', None)
 if SURVEY_EMAIL_FROM is None:
     SURVEY_EMAIL_FROM = 'donotreply@donotreply.com'
 
 # This site is for the notification emails that crowdsourcing sends when
 # a user enters a survey. The default is the site the user entered the survey
 # on.
-SURVEY_ADMIN_SITE = getattr(_gs, 'CROWDSOURCING_SURVEY_ADMIN_SITE', '')
+SURVEY_ADMIN_SITE = getattr(settings, 'CROWDSOURCING_SURVEY_ADMIN_SITE', '')
 
 # You can set a custom def oembed_expand(url, **opts) which takes the url to
 # a video and returns html embed code. Use the form path.to.my_function
-OEMBED_EXPAND = getattr(_gs, 'CROWDSOURCING_OEMBED_EXPAND', '')
+OEMBED_EXPAND = getattr(settings, 'CROWDSOURCING_OEMBED_EXPAND', '')
 
 # What URL should crowdsourcing redirect users to if they try to enter a survey
 # that requires a login?
-LOGIN_VIEW = getattr(_gs, 'CROWDSOURCING_LOGIN_VIEW', '')
+LOGIN_VIEW = getattr(settings, 'CROWDSOURCING_LOGIN_VIEW', '')
 
 # youtube has a lot of characters in their ids now so use [^&]
 # youtube also likes to add additional query arguments, so no trailing $
 # If you have oembed installed, crowdsourcing uses the oembed configuration and
 # ignores this.
 VIDEO_URL_PATTERNS = getattr(
-    _gs,
+    settings,
     'CROWDSOURCING_VIDEO_URL_PATTERNS',
     (r'^http://www\.youtube\.com/watch\?v=[^&]+',))
 
 # crowdsourcing.templatetags.crowdsourcing.google_map uses this setting.
 GOOGLE_MAPS_API_KEY = getattr(
-    _gs,
+    settings,
     'CROWDSOURCING_GOOGLE_MAPS_API_KEY',
     '')
 
@@ -99,7 +99,7 @@ GOOGLE_MAPS_API_KEY = getattr(
 # ImageWithThumbnailsField. For example, {'slideshow': {'size': (620, 350)}}
 # max_enlarge is in case users upload huge images that enlarge far too big.
 EXTRA_THUMBNAILS = {'max_enlarge': {'size': (1000, 1000)}}
-EXTRA_THUMBNAILS.update(getattr(_gs, 'CROWDSOURCING_EXTRA_THUMBNAILS', {}))
+EXTRA_THUMBNAILS.update(getattr(settings, 'CROWDSOURCING_EXTRA_THUMBNAILS', {}))
 
 # Syncing flickr synchronously means that crowdsourcing will attempt to sync on
 # save. This is not ideal because it makes a slow user experience, and failed
@@ -107,6 +107,6 @@ EXTRA_THUMBNAILS.update(getattr(_gs, 'CROWDSOURCING_EXTRA_THUMBNAILS', {}))
 # asynchronously synching is more difficult. crowdsourcing/tasks.py attempts to
 # set up a celery task.
 SYNCHRONOUS_FLICKR_UPLOAD = getattr(
-    _gs,
+    settings,
     'CROWDSOURCING_SYNCHRONOUS_FLICKR_UPLOAD',
     True)
